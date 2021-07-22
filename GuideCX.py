cd53711634f7d276e78966e4e04ab5455d209f1d
Luca Zhou <zhoumingcong6366@gmail.com>
2021-07-22 10:00:27 -0400

    feat: created a method to add a new note to a project
    1. Specified the url.
    2. Created the JSON body.
    3. Made the post request.
    
    Note: the `internalOnly` parameter is set to False by default - align to
    the documentation.

--- GuideCX.py
+++ GuideCX.py
@@ -26,9 +26,24 @@ class GuideCX:
         return response
 
     def getNote(self, noteID):
-        
+
         endpoint = f'/notes/{noteID}'
         url = self.HOST + endpoint
 
         response = requests.get(url, headers=self.head).json()
+        return response
+
+    def createNoteToProject(self, projectID, text, userEmail, internalOnly=False):
+        
+        endpoint = f'/projects/{projectID}/notes'
+        url = self.HOST + endpoint
+
+        body = {
+            'text': text,
+            'userEmail': userEmail,     # if !exists, project manager's email
+            'internalOnly': internalOnly
+        }
+
+        response = requests.post(url, json=body, headers=self.head).json()
+
         return response
